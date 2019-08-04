d973674ee75fba6a6e89fd1e6c8e38721137c180
Mobi-Ayub <mobiayub@gmail.com>
2019-08-04 19:45:37 +0800

    Added Google Cloud hosting
    Functionality added to allow the system to be hosted on google cloud

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var nodemailer = require('nodemailer');
 var Joi = require('joi');
 require('dotenv').config();
 
-var SVR_PORT = 3000;
+var SVR_PORT = 8080;
 
 var requestHandler = require('./requestHandlers');
 var database = require('./custom_modules/database-management');
@@ -345,6 +345,7 @@ app.post('/api/tags', function (req,res) {
     });
 
     res.send(tag);
+    emitter.emit('live map');
 });
 
 function validateTag(tag){
