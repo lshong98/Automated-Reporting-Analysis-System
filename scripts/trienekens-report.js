37d4e4c61dc7a1dc54071f8c4401b7731aafcaaf
FelixVincent12 <100079233@students.swinburne.edu.my>
2019-08-28 17:16:28 +0800

    change ifleetimg method
    - filter date collection date still got bug

--- scripts/trienekens-report.js
+++ scripts/trienekens-report.js
@@ -266,8 +266,6 @@ app.controller('dailyController', function ($scope, $window, $routeParams, $http
         }, 1000);
     });
     
-
-    
     $scope.addReport = function () {
         $scope.report.creationDate = $filter('date')(new Date(), 'yyyy-MM-dd HH:mm:ss');
         $scope.report.collectionDate = $filter('date')($scope.report.collectionDate, 'yyyy-MM-dd');
@@ -302,129 +300,193 @@ app.controller('dailyController', function ($scope, $window, $routeParams, $http
 //        $scope.report.ifleetImg = "";
 //    };
     
-    (function($) {
-        var defaults;
-        $.event.fix = (function(originalFix) {
-            return function(event) {
-                event = originalFix.apply(this, arguments);
-                if (event.type.indexOf("copy") === 0 || event.type.indexOf("paste") === 0) {
-                    event.clipboardData = event.originalEvent.clipboardData;
-                }
-                return event;
-            };
-        })($.event.fix);
-        defaults = {
-            callback: $.noop,
-            matchType: /image.*/
+//    (function($) {
+//        var defaults;
+//        $.event.fix = (function(originalFix) {
+//            return function(event) {
+//                event = originalFix.apply(this, arguments);
+//                if (event.type.indexOf("copy") === 0 || event.type.indexOf("paste") === 0) {
+//                    event.clipboardData = event.originalEvent.clipboardData;
+//                }
+//                return event;
+//            };
+//        })($.event.fix);
+//        defaults = {
+//            callback: $.noop,
+//            matchType: /image.*/
+//        };
+//        return ($.fn.pasteImageReader = function(options) {
+//            if (typeof options === "function") {
+//                options = {
+//                    callback: options
+//                };
+//            }
+//            options = $.extend({}, defaults, options);
+//            return this.each(function() {
+//                var $this, element;
+//                element = this;
+//                $this = $(this);
+//                return $this.bind("paste", function(event) {
+//                    var clipboardData, found;
+//                    found = false;
+//                    clipboardData = event.clipboardData;
+//                    return Array.prototype.forEach.call(clipboardData.types, function(type, i) {
+//                        var file, reader;
+//                        if (found) {
+//                            return;
+//                        }
+//                        if (
+//                            type.match(options.matchType) ||
+//                            clipboardData.items[i].type.match(options.matchType)
+//                        ) {
+//                            file = clipboardData.items[i].getAsFile();
+//                            reader = new FileReader();
+//                            reader.onload = function(evt) {
+//                                return options.callback.call(element, {
+//                                    dataURL: evt.target.result,
+//                                    event: evt,
+//                                    file: file,
+//                                    name: file.name
+//                                });
+//                            };
+//                            reader.readAsDataURL(file);
+//                            snapshoot();
+//                            return (found = true);
+//                        }
+//                    });
+//                });
+//            });
+//        });
+//    })(jQuery);
+//    
+//    var dataURL, filename;
+//    $("html").pasteImageReader(function(results) {
+//        filename = results.filename, dataURL = results.dataURL;
+//        $data.text(dataURL);
+//        $size.val(results.file.size);
+//        $type.val(results.file.type);
+//        var img = document.createElement("img");
+//        img.src = dataURL;
+//        var w = img.width;
+//        var h = img.height;
+//        $width.val(w);
+//        $height.val(h);
+//        return $(".active")
+//            .css({
+//                backgroundImage: "url(" + dataURL + ")"
+//            })
+//            .data({ width: w, height: h });
+//    });
+//
+//    var $data, $size, $type, $width, $height;
+//    $(function() {
+//        $data = $(".data");
+//        $size = $(".size");
+//        $type = $(".type");
+//        $width = $("#width");
+//        $height = $("#height");
+//        $(".target").on("click", function() {
+//            var $this = $(this);
+//            var bi = $this.css("background-image");
+//            if (bi != "none") {
+//                $data.text(bi.substr(4, bi.length - 6));
+//            }
+//
+//            $(".active").removeClass("active");
+//            $this.addClass("active");
+//
+//            //$this.toggleClass("contain");
+//
+//            $width.val($this.data("width"));
+//            $height.val($this.data("height"));
+//            if ($this.hasClass("contain")) {
+//                $this.css({
+//                    width: $this.data("500px"),
+//                    height: $this.data("height"),
+//                    "z-index": "10"
+//                });
+//            } else {
+//                $this.css({ width: "", height: "", "z-index": "" });
+//            }
+//        });
+//    });
+//
+//    function copy(text) {
+//        var t = document.getElementById("base64");
+//        t.select();
+//        try {
+//            var successful = document.execCommand("copy");
+//            var msg = successful ? "successfully" : "unsuccessfully";
+//            alert("Base64 data coppied " + msg + " to clipboard");
+//        } catch (err) {
+//            alert("Unable to copy text");
+//        }
+//    }
+    function retrieveImageFromClipboardAsBlob(pasteEvent, callback) {
+        if (pasteEvent.clipboardData == false) {
+            if (typeof(callback) == "function") {
+                callback(undefined);
+            }
         };
-        return ($.fn.pasteImageReader = function(options) {
-            if (typeof options === "function") {
-                options = {
-                    callback: options
-                };
+
+        var items = pasteEvent.clipboardData.items;
+
+        if (items == undefined) {
+            if (typeof(callback) == "function") {
+                callback(undefined);
             }
-            options = $.extend({}, defaults, options);
-            return this.each(function() {
-                var $this, element;
-                element = this;
-                $this = $(this);
-                return $this.bind("paste", function(event) {
-                    var clipboardData, found;
-                    found = false;
-                    clipboardData = event.clipboardData;
-                    return Array.prototype.forEach.call(clipboardData.types, function(type, i) {
-                        var file, reader;
-                        if (found) {
-                            return;
-                        }
-                        if (
-                            type.match(options.matchType) ||
-                            clipboardData.items[i].type.match(options.matchType)
-                        ) {
-                            file = clipboardData.items[i].getAsFile();
-                            reader = new FileReader();
-                            reader.onload = function(evt) {
-                                return options.callback.call(element, {
-                                    dataURL: evt.target.result,
-                                    event: evt,
-                                    file: file,
-                                    name: file.name
-                                });
-                            };
-                            reader.readAsDataURL(file);
-                            snapshoot();
-                            return (found = true);
-                        }
-                    });
-                });
-            });
-        });
-    })(jQuery);
-    
-    var dataURL, filename;
-    $("html").pasteImageReader(function(results) {
-        filename = results.filename, dataURL = results.dataURL;
-        $data.text(dataURL);
-        $size.val(results.file.size);
-        $type.val(results.file.type);
-        var img = document.createElement("img");
-        img.src = dataURL;
-        var w = img.width;
-        var h = img.height;
-        $width.val(w);
-        $height.val(h);
-        return $(".active")
-            .css({
-                backgroundImage: "url(" + dataURL + ")"
-            })
-            .data({ width: w, height: h });
-    });
+        };
 
-    var $data, $size, $type, $width, $height;
-    $(function() {
-        $data = $(".data");
-        $size = $(".size");
-        $type = $(".type");
-        $width = $("#width");
-        $height = $("#height");
-        $(".target").on("click", function() {
-            var $this = $(this);
-            var bi = $this.css("background-image");
-            if (bi != "none") {
-                $data.text(bi.substr(4, bi.length - 6));
+        for (var i = 0; i < items.length; i++) {
+            // Skip content if not image
+            if (items[i].type.indexOf("image") == -1) continue;
+            // Retrieve image on clipboard as blob
+            var blob = items[i].getAsFile();
+
+            if (typeof(callback) == "function") {
+                callback(blob);
             }
-
-            $(".active").removeClass("active");
-            $this.addClass("active");
-
-            //$this.toggleClass("contain");
-
-            $width.val($this.data("width"));
-            $height.val($this.data("height"));
-            if ($this.hasClass("contain")) {
-                $this.css({
-                    width: $this.data("500px"),
-                    height: $this.data("height"),
-                    "z-index": "10"
-                });
-            } else {
-                $this.css({ width: "", height: "", "z-index": "" });
-            }
-        });
-    });
-
-    function copy(text) {
-        var t = document.getElementById("base64");
-        t.select();
-        try {
-            var successful = document.execCommand("copy");
-            var msg = successful ? "successfully" : "unsuccessfully";
-            alert("Base64 data coppied " + msg + " to clipboard");
-        } catch (err) {
-            alert("Unable to copy text");
         }
     }
-    
+
+    window.addEventListener("paste", function(e) {
+
+        // Handle the event
+        retrieveImageFromClipboardAsBlob(e, function(imageBlob) {
+            // If there's an image, display it in the canvas
+            if (imageBlob) {
+                var canvas = document.getElementById("mycanvas");
+                var ctx = canvas.getContext('2d');
+
+                // Create an image to render the blob on the canvas
+                var img = new Image();
+
+                // Once the image loads, render the img on the canvas
+                img.onload = function() {
+                    // Update dimensions of the canvas with the dimensions of the image
+                    canvas.width = this.width;
+                    canvas.height = this.height;
+
+                    // Draw the image
+                    ctx.drawImage(img, 0, 0);
+                };
+
+                // Crossbrowser support for URL
+                var URLObj = window.URL || window.webkitURL;
+
+                // Creates a DOMString containing a URL representing the object given in the parameter
+                // namely the original Blob
+                img.src = URLObj.createObjectURL(imageBlob);
+                //console.log(imageBlob);
+                var reader = new FileReader();
+                reader.readAsDataURL(imageBlob); 
+                reader.onloadend = function() {
+                    var base64data = reader.result;                
+                    $scope.report.ifleetImg = base64data;
+                }
+            }
+        });
+    }, false);
 
 });
 
